453e6bffc6744cf29fe3c29cf2d57ef0c3d904a9
Marcy Sutton <marcy.sutton@deque.com>
2016-02-16 20:53:56 -0800

    Cleaning up test

--- test/unit/inject.js
+++ test/unit/inject.js
@@ -6,13 +6,6 @@ var Builder = require('../../lib/index');
 var sourceString = '/*! aXe v */\naxe.configure({"data":"something"});';
 
 describe('Attest', function () {
-	beforeEach(function(){
-		process.env.ATTEST_PATH = '../fixture/attest-config.json';
-	});
-	afterEach(function() {
-		delete process.env.ATTEST_PATH;
-	});
-
 	it('should assign optional source to this._source', function () {
 		var driver = sinon.stub();
 		assert.match(new Builder(driver, sourceString)._source, /\/\*! aXe/, 'regexp matches');
