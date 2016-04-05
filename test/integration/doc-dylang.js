0ed06a166d5650777dac130a8ed2f01c0b77b257
Dylan Barrell <dylan@barrell.com>
2016-04-05 12:03:56 -0400

    fix regressions due to 2.0.0 rule changes and bump dependency to 2.0.0

--- test/integration/doc-dylang.js
+++ test/integration/doc-dylang.js
@@ -7,53 +7,44 @@ var WebDriver = require('selenium-webdriver'),
 	json = require('../fixtures/attest-config.json'),
 	assert = require('chai').assert,
 	AxeBuilder = require('../../lib'),
-	fs = require('fs'),
-	colors = require('colors');
+	fs = require('fs');
 
-try {
-	var axe = require('../../../axe-bb/dist/index.js');
+	var axe = require('axe-core');
 
-	describe('doc-dylang.html', function () {
-		this.timeout(10000);
+describe('doc-dylang.html', function () {
+	this.timeout(10000);
 
-		var driver;
-		before(function (done) {
-			driver = new WebDriver.Builder()
-				.forBrowser('firefox')
-				.build();
+	var driver;
+	before(function (done) {
+		driver = new WebDriver.Builder()
+			.forBrowser('firefox')
+			.build();
 
-			driver
-				.get('http://localhost:9876/test/fixtures/doc-dylang.html')
-				.then(function () {
-					done();
-				});
-		});
-
-		after(function (done) {
-			driver.quit().then(function () {
+		driver
+			.get('http://localhost:9876/test/fixtures/doc-dylang.html')
+			.then(function () {
 				done();
 			});
-		});
-
-		it('should find violations with customized helpUrl', function (done) {
-			var src = '\naxe.configure(' + JSON.stringify(json) + ');';
-			src = axe.source + src;
-			AxeBuilder(driver, src)
-				.withRules(['dylang'])
-				.analyze(function (results) {
-					assert.lengthOf(results.violations, 1);
-					assert.equal(results.violations[0].id, 'dylang');
-					assert.notEqual(results.violations[0].helpUrl.indexOf('application=webdriverjs'), -1);
-					assert.lengthOf(results.passes, 0);
-					done();
-				});
-		});
-
 	});
-} catch (err) {
-	console.log('======================================================================='.red);
-	console.log('                                                                       '.red);
-	console.log('                   NO LOCAL AXE-CORE: TEST SKIPPED                     '.red);
-	console.log('                                                                       '.red);
-	console.log('======================================================================='.red);
-}
+
+	after(function (done) {
+		driver.quit().then(function () {
+			done();
+		});
+	});
+
+	it('should find violations with customized helpUrl', function (done) {
+		var src = '\naxe.configure(' + JSON.stringify(json) + ');';
+		src = axe.source + src;
+		AxeBuilder(driver, src)
+			.withRules(['dylang'])
+			.analyze(function (results) {
+				assert.lengthOf(results.violations, 1);
+				assert.equal(results.violations[0].id, 'dylang');
+				assert.notEqual(results.violations[0].helpUrl.indexOf('application=webdriverjs'), -1);
+				assert.lengthOf(results.passes, 0);
+				done();
+			});
+	});
+
+});
