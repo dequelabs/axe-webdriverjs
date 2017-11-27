b972fb314c593a32894d00ac668ac70a5d1ba0b7
Marcy Sutton <marcy.sutton@deque.com>
2017-11-27 12:32:15 -0800

    Sniff for shadow-dom in tests

--- test/integration/shadow-dom.js
+++ test/integration/shadow-dom.js
@@ -7,6 +7,8 @@ if (process.env.REMOTE_TESTSERVER_HOST) {
 	host = process.env.REMOTE_TESTSERVER_HOST;
 }
 
+var shadowSupported;
+
 describe('shadow-dom.html', function () {
 	this.timeout(10000);
 
@@ -17,7 +19,19 @@ describe('shadow-dom.html', function () {
 		driver
 			.get('http://' + host + ':9876/test/fixtures/shadow-dom.html')
 			.then(function () {
-				done();
+				driver.executeAsyncScript(function(callback) {
+					var script = document.createElement('script');
+					script.innerHTML = 'var shadowSupport = document.body && typeof document.body.attachShadow === \'function\';';
+					document.documentElement.appendChild(script);
+					callback(shadowSupport);
+				})
+				.then(function(shadowSupport) {
+					shadowSupported = shadowSupport;
+					done();
+				})
+				.catch(function(error) {
+					done();
+				});
 			});
 	});
 
@@ -28,13 +42,18 @@ describe('shadow-dom.html', function () {
 	});
 
 	it('should find violations', function (done) {
-		AxeBuilder(driver)
-			.analyze(function (results) {
-				assert.lengthOf(results.violations, 2);
-				assert.equal(results.violations[0].id, 'aria-roles');
-				assert.equal(results.violations[1].id, 'aria-valid-attr');
-				done();
-			});
+		if (shadowSupported) {
+			AxeBuilder(driver)
+				.analyze(function (results) {
+					assert.lengthOf(results.violations, 2);
+					assert.equal(results.violations[0].id, 'aria-roles');
+					assert.equal(results.violations[1].id, 'aria-valid-attr');
+					done();
+				});
+		} else {
+			console.log('Test skipped, Shadow DOM not supported');
+			done();
+		}
 	});
 
 });
