e2d2f77683cc8f39277654bc64e514440345c2c3
Stephen Mathieson <me@stephenmathieson.com>
2018-06-20 16:00:32 -0400

    fix: Prevent infinitely recursing when injecting into iframes
    This patch prevents `axe-webdriverjs` to infinitely recurse when injecting `axe-core` into `<iframe>`s. Instead of looping forever, we'll recurse 10 times before giving up. Closes #63.

---
+++ test/integration/gh-63-garmin.js
@@ -0,0 +1,24 @@
+var assert = require('chai').assert;
+var runWebdriver = require('../run-webdriver');
+var AxeBuilder = require('../..');
+
+describe('gh-63: buy.garmin.com/en-US/US/p/591046', function () {
+  this.timeout('1m');
+
+  var driver;
+  before(function () {
+    driver = runWebdriver();
+    return driver.get('https://buy.garmin.com/en-US/US/p/591046');
+  });
+
+  after(function () {
+    return driver.quit();
+  });
+
+  it('should not timeout', function (done) {
+    var axe = new AxeBuilder(driver);
+    axe.analyze(function () {
+      done();
+    });
+  });
+});
