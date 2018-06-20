4e54990a587bd9f8213542ac5b241f7c8c624255
Stephen Mathieson <me@stephenmathieson.com>
2018-06-20 16:05:40 -0400

    test: add longer `setScriptTimeout`

--- test/integration/gh-63-garmin.js
+++ test/integration/gh-63-garmin.js
@@ -8,6 +8,7 @@ describe('gh-63: buy.garmin.com/en-US/US/p/591046', function () {
   var driver;
   before(function () {
     driver = runWebdriver();
+    driver.manage().timeouts().setScriptTimeout(5000);
     return driver.get('https://buy.garmin.com/en-US/US/p/591046');
   });
 
