6f5b7bbe9997f675273031106c398873e010d3ac
Stephen Mathieson <me@stephenmathieson.com>
2018-06-22 09:29:35 -0400

    test: add flag to conditionally run GH-63 test

--- test/integration/gh-63.js
+++ test/integration/gh-63.js
@@ -1,27 +1,31 @@
 var runWebdriver = require('../run-webdriver');
 var AxeBuilder = require('../../lib');
 
-describe('gh-63: buy.garmin.com/en-US/US/p/591046', function() {
-  this.timeout('1m');
+var { RUN_THIRDPARTY_TESTS } = process.env;
 
-  var driver;
-  before(function() {
-    driver = runWebdriver();
-    driver
-      .manage()
-      .timeouts()
-      .setScriptTimeout(5000);
-    return driver.get('https://buy.garmin.com/en-US/US/p/591046');
-  });
+if (RUN_THIRDPARTY_TESTS) {
+  describe('gh-63: buy.garmin.com/en-US/US/p/591046', function() {
+    this.timeout('1m');
 
-  after(function() {
-    return driver.quit();
-  });
+    var driver;
+    before(function() {
+      driver = runWebdriver();
+      driver
+        .manage()
+        .timeouts()
+        .setScriptTimeout(5000);
+      return driver.get('https://buy.garmin.com/en-US/US/p/591046');
+    });
 
-  it('should not timeout', function(done) {
-    var axe = new AxeBuilder(driver);
-    axe.analyze(function() {
-      done();
+    after(function() {
+      return driver.quit();
+    });
+
+    it('should not timeout', function(done) {
+      var axe = new AxeBuilder(driver);
+      axe.analyze(function() {
+        done();
+      });
     });
   });
-});
+}
