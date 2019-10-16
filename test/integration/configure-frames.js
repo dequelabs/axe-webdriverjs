e73a6aa67ff557ad1836479a1cb2e40d4a9c0237
Paul Grenier <pgrenier@gmail.com>
2019-10-16 16:28:32 -0400

    fix: overrides sandbox attr on iframes

--- test/integration/configure-frames.js
+++ test/integration/configure-frames.js
@@ -66,3 +66,62 @@ describe('outer-configure-frame.html', function() {
       });
   });
 });
+
+describe('sandbox-outer-configure-frame.html', function() {
+  this.timeout(10000);
+
+  var server;
+  var driver;
+  before(function(done) {
+    driver = runWebdriver();
+    driver
+      .manage()
+      .timeouts()
+      .setScriptTimeout(10000);
+
+    server = createServer({
+      root: path.resolve(__dirname, '../..'),
+      cache: -1
+    });
+    server.listen(9876, err => {
+      if (err) {
+        return done(err);
+      }
+      driver
+        .get(
+          'http://' +
+            host +
+            ':9876/test/fixtures/sandbox-outer-configure-frame.html'
+        )
+        .then(function() {
+          done();
+        });
+    });
+  });
+
+  after(function() {
+    server.close();
+    driver.quit();
+  });
+
+  it('should find configured violations in all frames', function(done) {
+    AxeBuilder(driver)
+      .options({
+        rules: {
+          'landmark-one-main': { enabled: false },
+          'page-has-heading-one': { enabled: false },
+          region: { enabled: false },
+          'html-lang-valid': { enabled: false }
+        }
+      })
+      .configure(json)
+      .analyze()
+      .then(function(results) {
+        assert.equal(results.violations[0].id, 'dylang');
+        // the second violation is in a frame
+        assert.equal(results.violations[0].nodes.length, 2);
+
+        done();
+      });
+  });
+});
